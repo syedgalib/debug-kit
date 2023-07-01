ed4ad6a9aa3e68dc9494adf9b48d173413332049
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-07-01 19:01:08 +0600

    updated PHP version requirement

--- wp-logger.php
+++ wp-logger.php
@@ -14,7 +14,7 @@
  * Description:       A debugging tool.
  * Version:           0.1.0
  * Requires at least: 5.2
- * Requires PHP:      7.2
+ * Requires PHP:      7.4
  * Author:            Syed Galib
  * Author URI:        https://github.com/syedgalib
  * Text Domain:       wp-logger
