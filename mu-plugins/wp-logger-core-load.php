ec263591ca1c6d78c5458b09554c02fadae7c50e
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-07-02 16:26:35 +0600

    improved

--- mu-plugins/wp-logger-core-load.php
+++ mu-plugins/wp-logger-core-load.php
@@ -10,17 +10,17 @@
  *
  * @wordpress-plugin
  * Plugin Name:       WP Logger
- * Plugin URI:        https://github.com/syedgalib/wp-logger-core
+ * Plugin URI:        https://github.com/syedgalib/wp-logger/tree/main/mu-plugins/wp-logger-core
  * Description:       A debugging tool.
  * Version:           0.1.0
  * Requires at least: 5.2
- * Requires PHP:      7.2
+ * Requires PHP:      7.4
  * Author:            Syed Galib
  * Author URI:        https://github.com/syedgalib
  * Text Domain:       wp-logger
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Update URI:        https://github.com/syedgalib/wp-logger-core
+ * Update URI:        https://github.com/syedgalib/wp-logger/tree/main/mu-plugins/wp-logger-core
  */
 
 
